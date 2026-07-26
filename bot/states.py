4dd398f977e50651e18b06d40bbe853fc5888bcf
justik13 <honorplay2100@gmail.com>
2026-07-26 05:18:45 +0500

    fix

--- bot/states.py
+++ bot/states.py
@@ -10,11 +10,14 @@ class DeviceManagementStates(StatesGroup):
     rename_device = State()
 
 
+class PaymentStates(StatesGroup):
+    entering_receipt_email = State()
+
+
 class AdminStates(StatesGroup):
     searching_user = State()
     entering_broadcast_message = State()
     confirming_broadcast = State()
-
     adding_server = State()
     editing_server = State()
     confirming_server_delete = State()
@@ -22,9 +25,7 @@ class AdminStates(StatesGroup):
     editing_server_url = State()
     editing_server_key = State()
     editing_server_max_clients = State()
-
     editing_tariff_rub = State()
-
     admin_reducing_days = State()
     admin_extending_custom = State()
     admin_grant_custom_days = State()
