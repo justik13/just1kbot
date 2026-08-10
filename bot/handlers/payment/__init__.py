92d855776f7d07d8a16c46730ddb131398b05ac1
justik13 <honorplay2100@gmail.com>
2026-08-10 22:51:37 +0500

    fix: satisfy import ordering while preserving topup refresh compatibility

--- bot/handlers/payment/__init__.py
+++ bot/handlers/payment/__init__.py
@@ -1,3 +1,5 @@
+from importlib import import_module
+
 from aiogram import Router
 
 from services import account_topup as _account_topup
@@ -9,10 +11,10 @@ from services.account_topup_refresh import request_topup_status_refresh
 # balance router is imported.
 _account_topup.request_topup_status_refresh = request_topup_status_refresh
 
-from .showcase_routes import router as showcase_router
-from .balance_routes import router as balance_router
-from .purchase_routes import router as purchase_router
-from .tariff_change_routes import router as tariff_change_router
+showcase_router = import_module(".showcase_routes", __name__).router
+balance_router = import_module(".balance_routes", __name__).router
+purchase_router = import_module(".purchase_routes", __name__).router
+tariff_change_router = import_module(".tariff_change_routes", __name__).router
 
 router = Router()
 router.include_router(balance_router)
