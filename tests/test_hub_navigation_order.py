61a5aa7d23b2dbc6b1e6f1275e3fd83aa9b15606
justik13 <honorplay2100@gmail.com>
2026-08-11 08:05:15 +0500

    test: lock down hub action ordering

---
+++ tests/test_hub_navigation_order.py
@@ -0,0 +1,27 @@
+import unittest
+
+from bot.keyboards.common import get_hub_keyboard
+
+
+def callback_buttons(markup):
+    return [
+        button
+        for row in markup.inline_keyboard
+        for button in row
+        if button.callback_data
+    ]
+
+
+class HubNavigationOrderTests(unittest.TestCase):
+    def test_admin_action_is_last_and_white_internet_is_immediately_before_it(self):
+        buttons = callback_buttons(
+            get_hub_keyboard(is_admin=True, is_active=True)
+        )
+        callbacks = [button.callback_data for button in buttons]
+        self.assertEqual(callbacks[-2:], ["white_internet", "menu_admin"])
+
+    def test_white_internet_is_last_for_non_admin(self):
+        buttons = callback_buttons(
+            get_hub_keyboard(is_admin=False, is_active=True)
+        )
+        self.assertEqual(buttons[-1].callback_data, "white_internet")
