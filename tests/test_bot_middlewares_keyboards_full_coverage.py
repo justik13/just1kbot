caf7c10c39fe26866027ff0c954665a415cc3445
justik13 <honorplay2100@gmail.com>
2026-08-11 07:45:07 +0500

    test: remove obsolete profile keyboard coverage

--- tests/test_bot_middlewares_keyboards_full_coverage.py
+++ tests/test_bot_middlewares_keyboards_full_coverage.py
@@ -29,13 +29,9 @@ class TestBotMiddlewaresFullCoverage(unittest.IsolatedAsyncioTestCase):
     async def test_action_lock_middleware(self):
         middleware = ActionLockMiddleware()
         handler = AsyncMock(return_value="OK")
-
-        # Non-callback update
         msg = Message(message_id=1, date=123, chat=Chat(id=100, type="private"))
         res = await middleware(handler, msg, {})
         self.assertEqual(res, "OK")
-
-        # Callback update unlocked
         cb_user = TelegramUser(id=100, is_bot=False, first_name="Test")
         cb = CallbackQuery(id="1", from_user=cb_user, chat_instance="1", data="unlocked_action")
         res2 = await middleware(handler, cb, {})
@@ -44,15 +40,11 @@ class TestBotMiddlewaresFullCoverage(unittest.IsolatedAsyncioTestCase):
     async def test_ban_check_middleware(self):
         middleware = BanCheckMiddleware()
         handler = AsyncMock(return_value="OK")
-
-        # User is banned
         banned_user = MagicMock(is_banned=True)
         data = {"db_user": banned_user}
         msg = Message(message_id=1, date=123, chat=Chat(id=100, type="private"))
         res = await middleware(handler, msg, data)
         self.assertIsNone(res)
-
-        # User is not banned
         active_user = MagicMock(is_banned=False)
         data["db_user"] = active_user
         res2 = await middleware(handler, msg, data)
@@ -61,11 +53,9 @@ class TestBotMiddlewaresFullCoverage(unittest.IsolatedAsyncioTestCase):
     async def test_clean_chat_middleware(self):
         middleware = CleanChatMiddleware()
         handler = AsyncMock(return_value="OK")
-
         bot = AsyncMock()
         msg = Message(message_id=10, date=123, chat=Chat(id=100, type="private"))
         data = {"bot": bot}
-
         res = await middleware(handler, msg, data)
         self.assertEqual(res, "OK")
 
@@ -74,30 +64,22 @@ class TestBotMiddlewaresFullCoverage(unittest.IsolatedAsyncioTestCase):
         handler = AsyncMock(return_value="OK")
         bot = AsyncMock()
         msg = Message(message_id=11, date=123, chat=Chat(id=100, type="private"))
-
         direct_delete = AsyncMock()
         with patch("bot.middlewares.clean_chat._ensure_worker_started"), patch(
-            "bot.middlewares.clean_chat._delete_queue",
-            new=asyncio.Queue(maxsize=1),
-        ), patch(
-            "bot.middlewares.clean_chat._delete_message", direct_delete
-        ):
+            "bot.middlewares.clean_chat._delete_queue", new=asyncio.Queue(maxsize=1)
+        ), patch("bot.middlewares.clean_chat._delete_message", direct_delete):
             from bot.middlewares import clean_chat
-
             clean_chat._delete_queue.put_nowait((bot, 100, 10))
             res = await middleware(handler, msg, {"bot": bot})
             await asyncio.sleep(0.1)
-
         self.assertEqual(res, "OK")
         direct_delete.assert_awaited_once_with(msg.bot, 100, 11)
 
     async def test_correlation_middleware(self):
         middleware = CorrelationMiddleware()
         handler = AsyncMock(return_value="OK")
-
         msg = Message(message_id=1, date=123, chat=Chat(id=100, type="private"))
-        data = {}
-        res = await middleware(handler, msg, data)
+        res = await middleware(handler, msg, {})
         self.assertEqual(res, "OK")
 
     async def test_db_session_middleware(self):
@@ -105,7 +87,6 @@ class TestBotMiddlewaresFullCoverage(unittest.IsolatedAsyncioTestCase):
         handler = AsyncMock(return_value="OK")
         msg = Message(message_id=1, date=123, chat=Chat(id=100, type="private"))
         data = {}
-
         with patch("bot.middlewares.db_session.session_scope") as mock_scope:
             mock_session = AsyncMock()
             mock_scope.return_value.__aenter__.return_value = mock_session
@@ -116,13 +97,9 @@ class TestBotMiddlewaresFullCoverage(unittest.IsolatedAsyncioTestCase):
     async def test_private_chat_middleware(self):
         middleware = PrivateChatMiddleware()
         handler = AsyncMock(return_value="OK")
-
-        # Private chat
         msg = Message(message_id=1, date=123, chat=Chat(id=100, type="private"))
         res = await middleware(handler, msg, {})
         self.assertEqual(res, "OK")
-
-        # Group chat (ignored)
         msg_group = Message(message_id=1, date=123, chat=Chat(id=100, type="group"))
         res_g = await middleware(handler, msg_group, {})
         self.assertIsNone(res_g)
@@ -130,19 +107,16 @@ class TestBotMiddlewaresFullCoverage(unittest.IsolatedAsyncioTestCase):
     async def test_throttling_middleware(self):
         middleware = ThrottlingMiddleware()
         handler = AsyncMock(return_value="OK")
-
         user = TelegramUser(id=1001, is_bot=False, first_name="User")
         msg = Message(message_id=1, date=123, chat=Chat(id=1001, type="private"), from_user=user)
-
         res1 = await middleware(handler, msg, {})
         self.assertEqual(res1, "OK")
 
 
 class TestBotKeyboardsFullCoverage(unittest.TestCase):
     def test_user_keyboards(self):
-        kb_prof = user_kb.get_profile_keyboard()
-        self.assertIsNotNone(kb_prof)
-
+        kb_history = user_kb.get_history_keyboard()
+        self.assertIsNotNone(kb_history)
         kb_ref = user_kb.get_referral_keyboard(referral_link="https://t.me/bot?start=123")
         self.assertIsNotNone(kb_ref)
 
@@ -161,7 +135,6 @@ class TestBotKeyboardsFullCoverage(unittest.TestCase):
     def test_admin_keyboards(self):
         kb_admin_menu = admin_dashboard_kb.get_admin_menu()
         self.assertIsNotNone(kb_admin_menu)
-
         kb_admin_card = admin_users_kb.get_admin_user_card_keyboard(user_id=12345, is_banned=False)
         self.assertIsNotNone(kb_admin_card)
 
